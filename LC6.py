3d8c5196947b8c86e756bf3107337cf3731eb09a
earendil1412 <lisenpeng007@gmail.com>
2018-04-16 01:07:40 +0800

    ZigZag Conversion

---
+++ LC6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        result_arr = [""] * numRows
+        i = 0
+        circle = 2 * numRows - 2
+        while i < len(s):
+            j = i % circle
+            if j < numRows:
+                result_arr[j] += s[i]
+            else:
+                result_arr[numRows * 2 - j - 2] += s[i]
+            i += 1
+        result = ""
+        for item in result_arr:
+            result += item
+        return result
