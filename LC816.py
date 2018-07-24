65be2d031826fc6458ae02fa98d9f82dd982d5ac
Earendil1412 <lisenpeng007@gmail.com>
2018-07-25 00:22:07 +0800

    tweak

--- LC816.py
+++ LC816.py
@@ -8,56 +8,47 @@ class Solution:
         index = 0
         flag = True
         S = S[1:-1]
-        for i in S:
+        for _ in S:
             if index == 0 or index == len(S):
                 index = index + 1
                 continue
             else:
-                a = S[:index]
-                b = S[index:]
+                a = __add_dot__(S[:index])
+                b = __add_dot__(S[index:])
                 index = index + 1
-                x = 1
-                for _ in a:
-                    xa = ""
-                    if len(float_to_str(float(a) / x)) - 1 == len(a):
-                        xa = float_to_str(float(a) / x)
-                    x = x * 10
-                    y = 1
-                    for _ in b:
-                        xb = ""
-                        if len(float_to_str(float(b) / y)) - 1 == len(b):
-                            xb = float_to_str(float(b) / y)
-                        y = y * 10
-                        if xa == "" and xb == "":
-                            continue
-                        if xa != "" and xb != "" and float(xa.split(".")[1]) != 0 and float(xb.split(".")[1]) != 0:
-                            result.append("(" + xa + ", " + xb + ")")
-                            continue
-                        if xa == "" and xb != "" and float(xb.split(".")[1]) != 0 and len(a) == len(str(int(a))):
-                            result.append("(" + a + ", " + xb + ")")
-                            continue
-                        if xb == "" and xa != "" and float(xa.split(".")[1]) != 0 and len(b) == len(str(int(b))):
-                            result.append("(" + xa + ", " + b + ")")
-                            continue
-                if len(a) == len(str(int(a))) and len(b) == len(str(int(b))):
-                    result.append("(" + a + ", " + b + ")")
+                if len(a) > 0 and len(b) > 0:
+                    for ia in a:
+                        for ib in b:
+                            result.append("(" + ia + ", " + ib + ")")
 
-        return list(set(result))
+        return result
 
-def float_to_str(f):
-    float_string = repr(f)
-    if 'e' in float_string:  # detect scientific notation
-        digits, exp = float_string.split('e')
-        digits = digits.replace('.', '').replace('-', '')
-        exp = int(exp)
-        zero_padding = '0' * (abs(int(exp)) - 1)  # minus 1 for decimal point in the sci notation
-        sign = '-' if f < 0 else ''
-        if exp > 0:
-            float_string = '{}{}{}.0'.format(sign, digits, zero_padding)
+
+def __add_dot__(S):
+    """
+    :type S: str
+    :rtype: List[str]
+    """
+    if len(S) == 1:
+        return [S]
+    if S[0] == "0" and S[len(S) - 1] == "0":
+        return []
+    if S[0] == "0":
+        return [S[0] + "." + S[1:]]
+    if S[len(S) - 1] == "0":
+        return [S]
+    result = [S]
+    index = 0
+    for _ in S:
+        if index == 0 or index == len(S):
+            index = index + 1
+            continue
         else:
-            float_string = '{}0.{}{}'.format(sign, zero_padding, digits)
-    return float_string
+            result.append(S[:index] + "." + S[index:])
+            index = index + 1
+    return result
+
 
 solution = Solution()
-print(solution.ambiguousCoordinates("(123)"))
+print(solution.ambiguousCoordinates("(0123)"))
 # print(len(123))
